eb642d34e530a3802e5a343dec19900d3fc07b12
kawanet <u-suke@kawa.net>
2015-07-26 01:47:18 +0900

    benchmark in 2 seconds

--- bench/bench.js
+++ bench/bench.js
@@ -7,54 +7,54 @@ var msgpack5 = require("msgpack5")();
 var msgpack_unpack = require("msgpack-unpack");
 
 var data = require("./example");
+var packed = msgpack_lite.encode(data);
+var expected = JSON.stringify(data);
 
-var biggest = 100000;
-var bigger = 10000;
-var smaller = 1000;
+var limit = process.argv[2] - 0 || 2;
+limit *= 1000;
 
 var ret;
-var packed;
 
-ret = bench('JSON.stringify()                  ', JSON.stringify, data, biggest);
-ret = bench('JSON.parse()                      ', JSON.parse, ret, biggest);
+ret = bench('JSON.stringify()                  ', JSON.stringify, data);
+ret = bench('JSON.parse()                      ', JSON.parse, ret);
 test(ret, data);
 
-ret = bench('require("msgpack").pack()         ', msgpack_node.pack, data, biggest);
-ret = bench('require("msgpack").unpack()       ', msgpack_node.unpack, ret, biggest);
+ret = bench('require("msgpack").pack()         ', msgpack_node.pack, data);
+ret = bench('require("msgpack").unpack()       ', msgpack_node.unpack, ret);
 test(ret, data);
 
-ret = bench('require("msgpack-lite").encode()  ', msgpack_lite.encode, data, bigger);
-packed = ret;
-ret = bench('require("msgpack-lite").decode()  ', msgpack_lite.decode, ret, bigger);
+ret = bench('require("msgpack-lite").encode()  ', msgpack_lite.encode, data);
+ret = bench('require("msgpack-lite").decode()  ', msgpack_lite.decode, ret);
 test(ret, data);
 
-ret = bench('require("msgpack-js").encode()    ', msgpack_js.encode, data, bigger);
-ret = bench('require("msgpack-js").decode()    ', msgpack_js.decode, ret, bigger);
+ret = bench('require("msgpack-js").encode()    ', msgpack_js.encode, data);
+ret = bench('require("msgpack-js").decode()    ', msgpack_js.decode, ret);
 test(ret, data);
 
-ret = bench('require("msgpack5")().encode()    ', msgpack5.encode, data, smaller);
-ret = bench('require("msgpack5")().decode()    ', msgpack5.decode, ret, smaller);
+ret = bench('require("msgpack5")().encode()    ', msgpack5.encode, data);
+ret = bench('require("msgpack5")().decode()    ', msgpack5.decode, ret);
 test(ret, data);
 
-ret = bench('require("msgpack-unpack").decode()', msgpack_unpack, packed, smaller);
+ret = bench('require("msgpack-unpack").decode()', msgpack_unpack, packed);
 test(ret, data);
 
 function bench(name, func, src, count) {
+  var ret, duration;
   var start = new Date() - 0;
-  var ret;
-  for (var i = 0; i < count; i++) {
-    ret = func(src)
+  var count = 0;
+  while (1) {
+    var end = new Date() - 0;
+    duration = end - start;
+    if (duration >= limit) break;
+    while ((++count) % 100) ret = func(src)
   }
-  var end = new Date() - 0;
-  var duration = end - start;
   var score = Math.floor(count / duration * 100);
   console.warn(name, count + "op", "/", duration + "ms", "=", score, "op/ms");
   return ret;
 }
 
-function test(actual, expected) {
+function test(actual) {
   actual = JSON.stringify(actual);
-  expected = JSON.stringify(expected);
   if (actual === expected) return;
   console.warn("expected: " + expected);
   console.warn("actual:   " + actual);
