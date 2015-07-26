2b6e2bf87f047fdda121d50363ba9c6a162eaa92
kawanet <u-suke@kawa.net>
2015-07-26 15:15:51 +0900

    createEncodeStream and createDecodeStream added

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 
 exports.encode = require("./lib/encode").encode;
 exports.decode = require("./lib/decode").decode;
+
+exports.createEncodeStream = require("./lib/encode-stream").createEncodeStream;
+exports.createDecodeStream = require("./lib/decode-stream").createDecodeStream;
