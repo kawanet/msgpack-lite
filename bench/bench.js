c435de43d0eb802dd38b6858023f6d6ad4c846c5
kawanet <u-suke@kawa.net>
2015-07-27 20:07:10 +0900

    Speed Comparison

--- bench/bench.js
+++ bench/bench.js
@@ -13,12 +13,15 @@ var expected = JSON.stringify(data);
 
 var argv = Array.prototype.slice.call(process.argv, 2);
 
-var limit = 2;
+var limit = 5;
 if (argv[0] - 0) limit = argv.shift() - 0;
 limit *= 1000;
 
 var grep;
 if (argv[0]) grep = argv.shift();
+
+console.log("operation                                   ", "|", "result            ", "|", "op/ms");
+console.log("--------------------------------------------", "|", "------------------", "|", "-----");
 
 var buf, obj;
 
@@ -46,7 +49,7 @@ buf = bench('buf = require("msgpack5")().encode(obj);    ', msgpack5.encode, dat
 obj = bench('obj = require("msgpack5")().decode(buf);    ', msgpack5.decode, buf);
 test(obj);
 
-obj = bench('require("msgpack-unpack").decode()', msgpack_unpack, packed);
+obj = bench('obj = require("msgpack-unpack").decode(buf);', msgpack_unpack, packed);
 test(obj);
 
 function JSON_stringify(src) {
@@ -65,7 +68,9 @@ function bench(name, func, src) {
     while ((++count) % 100) ret = func(src);
   }
   var score = Math.floor(count / duration * 100);
-  console.warn(name, count + "op", "/", duration + "ms", "=", score, "op/ms");
+  var col = count + "op" + " / " + duration + "ms";
+  col += "                  ".substr(0, 18 - col.length);
+  console.log(name, "|", col, "|", score);
   return ret;
 }
 
